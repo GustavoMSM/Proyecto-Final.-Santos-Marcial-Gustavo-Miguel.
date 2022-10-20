e02c8a30d0cde993a2bbfbaf3f91bc048110b3a9
GustavoMSM <114272368+GustavoMSM@users.noreply.github.com>
2022-10-19 18:16:42 -0700

    Update database.js

--- Proyecto Final de desarollo de software. Santos Marcial Gustavo Miguel/src/database.js
+++ Proyecto Final de desarollo de software. Santos Marcial Gustavo Miguel/src/database.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://GustavoSantosCESUN:<password>@cluster1.5c8bnjr.mongodb.net/?retryWrites=true&w=majority";
+const uri = "mongodb+srv://GustavoSantosCESUN:ufknCYpkUqkisH0a@cluster1.5c8bnjr.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
